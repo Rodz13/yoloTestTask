a94f4cfacace239a1b095bb394c7f84eded5fa0c
Rodrigo <rodrigomirapalheta@hotmail.com>
2022-11-26 16:56:54 +0200

    add pre-commit hook

--- country-picker/src/App.tsx
+++ country-picker/src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { ApolloProvider } from '@apollo/client';
 import 'antd/dist/reset.css';
 
-
 import config from './lib/apollo';
 
 import { Header } from './components/Header';
